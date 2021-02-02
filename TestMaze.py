b4b6ad96e159c6dc435bbc5991f16c0723cca231
Tejas Sameera <sameera.tejas5@gmail.com>
2021-02-01 19:02:03 -0500

    a_star

--- TestMaze.py
+++ TestMaze.py
@@ -9,7 +9,8 @@ import pandas as pd
 import random
 from collections import deque
 from copy import copy, deepcopy
-
+from queue import PriorityQueue
+from dataclasses import dataclass, field
 
 # queue node used in BFS
 class Node:
@@ -22,6 +23,21 @@ class Node:
  
     def __repr__(self):
         return str((self.x, self.y))
+class ENode:
+    # (x, y) represents coordinates of a cell in matrix
+    # maintain a parent node for printing path
+    def __init__(self, x, y, parent,distance,priority):
+        self.x = x
+        self.y = y
+        self.parent = parent
+        self.distance = distance
+        self.priority = priority
+    def __lt__(self, other):
+        return self.priority < other.priority
+    def __repr__(self):
+        return str((self.x, self.y))
+
+
 
 row = [-1, 0, 0, 1]
 col = [0, -1, 1, 0]
@@ -54,8 +70,14 @@ def printMatrix(Matrix):
 
 
 testMatrix = createMatrix(10, .24)
+dfsTestMatrix = deepcopy(testMatrix)
+astar_testMatrix = deepcopy(testMatrix)
+
 bfsPathMatrix = deepcopy(testMatrix)
-printMatrix(testMatrix)
+dfsPathMatrix = deepcopy(testMatrix)
+astarPathMatrix = deepcopy(testMatrix)
+
+#printMatrix(testMatrix)
 '''
 for i in range(len(testMatrix)):
     for j in range(len(testMatrix[0])):
@@ -88,19 +110,19 @@ def advance_fire(curr_matrix):
 dfsPath = []
 def DFSsearch(Coord1, Coord2, Matrix):
     stack = []
-   
+    start = Node(Coord1[0],Coord1[1],None)
     if (Matrix[Coord2[0]][Coord2[1]] == '!') or (Matrix[Coord1[0]][Coord1[1]] == '!') or (Matrix[Coord1[0]][Coord1[1]] == '_') or (Matrix[Coord1[0]][Coord1[1]] == '_'):
         return False
  
     
-    stack.append(Coord1)
+    stack.append(start)
     
-    while(len(stack)):
+    while stack:
         
-        s = stack.pop()
-      
-        xPos = int(s[0])
-        yPos = int(s[1])
+        curr = stack.pop()
+
+        xPos = int(curr.x)
+        yPos = int(curr.y)
        
         #Update Maze for Fire
 
@@ -109,18 +131,18 @@ def DFSsearch(Coord1, Coord2, Matrix):
             #Compare the value  in the given matrix location
         # 3. Boundries
             #Compare the x and y boundries
-        if ( xPos < 0  or xPos >= len(Matrix) or yPos < 0 or yPos >= len(Matrix[0])  or ((Matrix[s[0]][s[1]]) == '1') or ((Matrix[s[0]][s[1]]) == '_')):
+        if ( xPos < 0  or xPos >= len(Matrix) or yPos < 0 or yPos >= len(Matrix[0])  or ((Matrix[curr.x][curr.y]) == '1') or ((Matrix[curr.x][curr.y]) == '_')):
             continue
 
         #Also update fire??
         Matrix[xPos][yPos] = '1'
         
         if (Matrix[Coord2[0]][Coord2[1]] == '!'):
-            return False
+            return None
     
     
         if (xPos == Coord2[0] and yPos == Coord2[1]):
-            return True
+            return curr
         
         #print(str(xPos) + "\t" + str(yPos))
             
@@ -130,12 +152,19 @@ def DFSsearch(Coord1, Coord2, Matrix):
         #Check Right 
         #Check Left
        
+        for i in range(4):
+            x = xPos + row[i]
+            y = yPos + col[i]
+            next = Node(x,y,curr)
+            stack.append(next)
+
+        '''
         stack.append((xPos + 1,yPos))
         stack.append((xPos -1, yPos))
         stack.append((xPos, yPos + 1))
         stack.append((xPos, yPos - 1))
-        
-    return False
+        '''
+    return None
 
 def BFS(Coord1, Coord2, Matrix):
     q = deque()
@@ -164,6 +193,8 @@ def BFS(Coord1, Coord2, Matrix):
         if ( xPos < 0  or xPos >= len(Matrix) or yPos < 0 or yPos >= len(Matrix[0])  or ((Matrix[curr.x][curr.y]) == '1') or ((Matrix[curr.x][curr.y]) == '_')):
             continue
 
+        if (xPos == Coord2[0] and yPos == Coord2[1]):
+            return curr
         
         Matrix[xPos][yPos] = '1'
         
@@ -171,8 +202,7 @@ def BFS(Coord1, Coord2, Matrix):
             return None
     
     
-        if (xPos == Coord2[0] and yPos == Coord2[1]):
-            return curr
+       
         
         #print(str(xPos) + "\t" + str(yPos))
             
@@ -196,6 +226,61 @@ def BFS(Coord1, Coord2, Matrix):
         '''
     return None
     
+def heuristic(pointA, pointB):
+    point1 = np.array(pointA) 
+    point2 = np.array(pointB) 
+  
+    # calculating Euclidean distance 
+    # using linalg.norm() 
+    hue = np.linalg.norm(point1 - point2) 
+    return hue
+
+def a_star(Coord1, Coord2, Matrix):
+
+    pq = PriorityQueue()
+    start = ENode(Coord1[0],Coord1[1],None,0,0)
+
+    if (Matrix[Coord2[0]][Coord2[1]] == '!') or (Matrix[Coord1[0]][Coord1[1]] == '!') or (Matrix[Coord1[0]][Coord1[1]] == '_') or (Matrix[Coord1[0]][Coord1[1]] == '_'):
+        return None
+
+    pq.put(start)
+
+    while not pq.empty():
+        curr = pq.get()
+       
+        xPos = (curr.x)
+        yPos = (curr.y)
+       
+        #Update Maze for Fire
+
+        # 1. Visited 
+        # 2. Barrier
+            #Compare the value  in the given matrix location
+        # 3. Boundries
+            #Compare the x and y boundries
+        if ( xPos < 0  or xPos >= len(Matrix) or yPos < 0 or yPos >= len(Matrix[0])  or ((Matrix[curr.x][curr.y]) == '1') or ((Matrix[curr.x][curr.y]) == '_')):
+            continue
+
+        if (xPos == Coord2[0] and yPos == Coord2[1]):
+            return curr
+        
+        Matrix[xPos][yPos] = '1'
+        
+        if (Matrix[Coord2[0]][Coord2[1]] == '!'):
+            return None
+
+        for i in range(4):
+            x = xPos + row[i]
+            y = yPos + col[i]
+           
+            hue = heuristic((x,y),Coord2)
+            dist = (curr.parent.distance if curr.parent else 0)  + 1
+            cost = hue  + dist
+            next = ENode(x,y,curr,dist,cost)
+            pq.put(next)
+
+    return None
+
 def getPath(node,matrix):
     stack = []
     temp = matrix
@@ -208,20 +293,39 @@ def getPath(node,matrix):
    
     while stack:
         print(stack.pop(), end = ' ') 
+   
     print("")
     return temp
 #print(DFSsearch(a, b, testMatrix))
-
-temp = BFS(a,b,testMatrix)
-if temp: 
-    print(testMatrix)
+print(dfsTestMatrix)
+bfsTemp = BFS(a,b,testMatrix)
+dfsTemp = DFSsearch(a,b,dfsTestMatrix)
+astarTemp = a_star(a,b,astar_testMatrix)
+'''
+if dfsTemp:
+  
     print("Path: ")
-    l = getPath(temp,bfsPathMatrix)
+    l = getPath(dfsTemp,dfsPathMatrix)
     print(l)
 else:
     print("no path")
 
+if bfsTemp: 
+   
+    print("Path: ")
+    l = getPath(bfsTemp,bfsPathMatrix)
+    print(l)
+else:
+    print("no path")
+'''
+if astarTemp:
+    print("Path: ")
+    l = getPath(astarTemp,astarPathMatrix)
+    print(l)
+else:
+    print("no path")
 
+print(heuristic(a,b))
 
 
 
